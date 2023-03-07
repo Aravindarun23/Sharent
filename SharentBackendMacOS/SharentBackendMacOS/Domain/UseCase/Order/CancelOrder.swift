eedfce79e4cfd5bff24e273db57e7e1c1b59fd1d
Aravind G <aravind.govind@zohocorp.com>
2023-03-08 00:27:07 +0530

    add cancel order

---
+++ SharentBackendMacOS/SharentBackendMacOS/Domain/UseCase/Order/CancelOrder.swift
@@ -0,0 +1,27 @@
+//
+//  CancelOrder.swift
+//  SharentBackendMacOS
+//
+//  Created by aravind-pt6209 on 08/03/23.
+//
+
+import Foundation
+import VTComponents
+
+
+public final class CancelOrderRequest: ZRequest {
+    
+}
+
+public final class CancelOrderResponse: ZResponse {
+    
+}
+
+public final class CancelOrderError: ZError {
+    
+}
+
+
+public final class CancelOrder: ZUsecase<CancelOrderRequest, CancelOrderResponse, CancelOrderError> {
+    
+}
